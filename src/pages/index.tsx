8f8da52db86eb3a05063b102e94900a7d315c349
Lorris Saint-Genez <lorrissaintgenez@gmail.com>
2023-01-12 13:55:22 -0800

    fix: move id creation outside of getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import crypto from 'crypto';
 
+const id = crypto.randomBytes(16).toString('hex');
+
 const Index = () => (
   <div>
     <h1>Hello!</h1>
@@ -11,10 +13,7 @@ const Index = () => (
 const REVALIDATE_DELAY = 30;
 
 export function getStaticProps() {
-  const id = crypto.randomBytes(16).toString('hex');
-
   console.log(id);
-
   return {
     props: {
       test: 'hi',
