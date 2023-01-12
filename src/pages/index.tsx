4fb414783b5f26a60ae0b60e36d30258ba224eed
Lorris Saint-Genez <lorrissaintgenez@gmail.com>
2023-01-11 16:56:24 -0800

    fix: remove console.log

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 
-const REVALIDATE_TIME_IN_S = 30;
-
 const Index = () => <div>hi</div>;
 
-export function getStaticProps() {
-  console.log(
-    `Hello there, are you really called every ${REVALIDATE_TIME_IN_S} seconds?`
-  );
+const REVALIDATE_DELAY = 30;
 
+export function getStaticProps() {
   return {
     props: {
       test: 'hi',
     },
-    revalidate: REVALIDATE_TIME_IN_S, // In seconds
+    revalidate: REVALIDATE_DELAY, // In seconds
   };
 }
 
