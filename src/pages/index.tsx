5bce81f5b71377a49dbf48721c3d593fdfa3ac9c
Lorris Saint-Genez <lorrissaintgenez@gmail.com>
2023-01-12 08:26:49 -0800

    use ID to check getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import crypto from 'crypto';
 
 const Index = () => <div>hi</div>;
 
 const REVALIDATE_DELAY = 30;
 
 export function getStaticProps() {
+  const id = crypto.randomBytes(16).toString('hex');
+
+  console.log(id);
+
   return {
     props: {
       test: 'hi',
